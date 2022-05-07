0da51f69f89751d609293a612329892e96df05b3
samer.karam <smk.orcl@gmail.com>
2022-05-07 18:54:20 +0200

    env var

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -2,3 +2,5 @@ eb init udagram-api --platform node.js --region us-east-1
 eb create --sample udagram-api-dev
 eb use udagram-api-dev
 eb deploy udagram-api-dev
+eb setenv DB_PORT=$DB_PORT JWT_SECRET=$JWT_SECRET PORT=$PORT POSTGRES_DB=$POSTGRES_DB POSTGRES_HOST=$POSTGRES_HOST POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_USERNAME=$POSTGRES_USERNAME
+URL=$URL
